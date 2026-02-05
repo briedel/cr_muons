bdff8edcb17b508a12edcf2c058b6d335fbcbaea
Benedikt Riedel <benedikt.riedel@gmail.com>
2026-02-05 15:52:21 -0600

    minor changes to extraction submit scripts

--- utils/condor/convert_data/submit.sh
+++ utils/condor/convert_data/submit.sh
@@ -8,4 +8,14 @@
 
 range=$(dirname $1 | awk -F/ '{print $(NF-1)}')
 
-/cvmfs/icecube.opensciencegrid.org/py3-v4.4.2/icetray-env icetray/v1.17.0 python3 /home/briedel/code/cr_muons_git/utils/dump_muonitron_data.py --infile $1 --outfile /data/sim/IceCube/2025/testing/$range/$(basename $1).parquet --format parquet
+out=/data/sim/IceCube/2025/testing/$range/$(basename $1).parquet
+
+echo $range
+echo $out
+
+if [[ -f "$out" ]]; then
+    echo "File $out exists. Exit 0"
+    exit 0
+fi 
+
+/cvmfs/icecube.opensciencegrid.org/py3-v4.4.2/icetray-env icetray/v1.17.0 python3 /home/briedel/code/cr_muons_git/utils/data_converters/dump_muonitron_data.py --infile $1 --outfile $out  --format parquet
